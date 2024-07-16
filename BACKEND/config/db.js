8ab8c952755d6d0e0b6af2ef7e7fa0d94330db48
michaelnadar <michealajit1@gmail.com>
2024-07-16 18:57:35 +0530

    adsa

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            // Remove useCreateIndex and useFindAndModify options
-        });
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
